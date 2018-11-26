55a8c34aecdb77772df052ae186d45c91940fed3
Gromoboy <amejuev@gmail.com>
2018-11-27 00:14:39 +0300

    восстановление пропавших файлов

--- lesson1/Menu.js
+++ lesson1/Menu.js
@@ -14,7 +14,6 @@ class Menu {
             if (this.items[i] instanceof SubMenu){
               result = result.slice(0,-5) + this.items[i].render() + result.slice(-5);
 
-              // result += this.items[i].render();
             }
         }
         result += `</ul>`;
@@ -31,7 +30,5 @@ class Menu {
 }
 
 class SubMenu extends Menu {
-  // render() {
-  //   return `<ul>${super.render()}`;
-  // }
+
 }
