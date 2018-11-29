2b6e80f2b80f58f3341d3dfc7fc1644d06c98f7f
Gromoboy <amejuev@gmail.com>
2018-11-29 10:28:24 +0300

    переделан как на лекции

--- lesson1/Menu.js
+++ lesson1/Menu.js
@@ -7,14 +7,14 @@ class Menu {
     render(){
         let result = `<ul id="${this.id}" class="${this.className}">`;
         for (let i = 0; i < this.items.length; i++){
-            if(this.items[i] instanceof MenuItem) {
+            if(this.items[i] instanceof MenuItem || this.items[i] instanceof SubMenu) {
                 result += this.items[i].render();
             }
             //TODO: instanceof SubMenu
-            if (this.items[i] instanceof SubMenu){
-              result = result.slice(0,-5) + this.items[i].render() + result.slice(-5);
-
-            }
+            // if (this.items[i] instanceof SubMenu){
+            //   result = result.slice(0,-5) + this.items[i].render() + result.slice(-5);
+            //
+            // }
         }
         result += `</ul>`;
         return result
@@ -30,5 +30,12 @@ class Menu {
 }
 
 class SubMenu extends Menu {
-
+  constructor(href, title, id, className, items) {
+    super(id, className, items);
+    this.href = href;
+    this.title = title;
+  }
+  render() {
+    return `<li><a href="${this.href}">${this.title}</a>${super.render()}</li>`
+  }
 }
